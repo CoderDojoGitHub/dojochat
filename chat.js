5ebcadf6e89fc82e15726156ad0037f4670f50c0
Kristján Pétursson <kristjan@gmail.com>
2013-01-20 19:43:28 -0800

    Start talking to PubNub
    Basic publish and subscribe via the PubNub JS library. PubNub's easiest setup
    puts the keys into an HTML element and relies on global config, but let's not go
    teaching any bad habits.

--- chat.js
+++ chat.js
@@ -1,4 +1,28 @@
+/*global PUBNUB:true*/
+
+var pubnub;
+
+function setUpPubNub() {
+  pubnub = PUBNUB.init({
+    publish_key   : "PUBLISH_KEY",
+    subscribe_key : "SUBSCRIBE_KEY"
+  });
+
+  pubnub.subscribe({
+    channel : "dojochat",
+    message : function(m){
+      console.log(m);
+    },
+    connect : function(channel) {
+      pubnub.publish({
+        channel : "dojochat",
+        message : "Hi"
+      });
+    }
+  });
+}
+
 $(function() {
-  console.log("Ready");
+  setUpPubNub();
 });
 
